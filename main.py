390ed827280567d99ac56de3dedf93f07267b4e6
legalchik <ishmiev2005@gmail.com>
2022-11-12 13:17:24 +0300

    edit

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from flask import Flask, request
 import os
 from git import Git
 
-exit()
+
 app = Flask(__name__)
 
 
-@app.route('/update_server', methods=['POST'])
+@app.route('/u', methods=['GET'])
 def webhook():
-    if request.method == 'POST':
-        DIR = os.getcwd()
-        res = Git(DIR).pull()
-        return 'Updated PythonAnywhere successfully: '+str(res), 200
-    else:
-        return 'Wrong event type', 400
+    DIR = os.getcwd()
+    res = Git(DIR).pull()
+    return 'Updated PythonAnywhere successfully: '+str(res), 200
+
+
+if __name__ == "__main__":
+    app.run()
